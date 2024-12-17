26995c4e6aa3e679c0090f33faf06021a7ef8bc2
LiYuHan <C112156105@nkust.edu.tw>
2024-12-17 15:22:19 +0800

    text

--- gemini.py
+++ gemini.py
@@ -1,6 +1,7 @@
 import google.generativeai as generativeai
 import os
 
+# Set the API key
 api_key = os.getenv("gemini_api_key")
 generativeai.configure(api_key=api_key)
 response = generativeai.GenerativeModel("gemini-2.0-flash-exp").generate_content("生命的意義是甚麼？")
