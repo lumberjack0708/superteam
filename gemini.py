cc52fcb43b666ab434dadcbcdb18182f0d254f1f
tingjunchen425 <C112156101@nkust.edu.tw>
2024-12-17 15:24:03 +0800

    set message to gemini

--- gemini.py
+++ gemini.py
@@ -3,5 +3,6 @@ import os
 
 api_key = os.getenv("gemini_api_key")
 generativeai.configure(api_key=api_key)
-response = generativeai.GenerativeModel("gemini-2.0-flash-exp").generate_content("生命的意義是甚麼？")
+text = "生命的意義是甚麼？"
+response = generativeai.GenerativeModel("gemini-2.0-flash-exp").generate_content(text)
 print(response.text)
